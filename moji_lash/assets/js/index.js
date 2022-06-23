bbda3fc80f680197ed6348dadb952d637d0f7c14
kohakubaibai <51185142+kohakubaibai@users.noreply.github.com>
2022-06-23 18:28:13 +0800

    MOJI-formula html and js

--- moji_lash/assets/js/index.js
+++ moji_lash/assets/js/index.js
@@ -46,4 +46,32 @@ $(function () {
 		nextArrow: '<button class="slick-next slides__arrow slides__arrow--teacher next" aria-label="Next" type="button"></button>',
 		dotsClass: 'slides__dots slides__dots--teacher'
 	});
+
+	var formulaGroup = '';
+	for(let i = 0; i < formula.length; i++) {
+		var formulaItem = '';
+		formulaItem += '<div class="category"><div class="category__main"><div class="category__head"><div class="category__name"><span class="en didot">' + formula[i]["formula_en"] +'</span><span class="ch">' + formula[i]["formula_ch"] +'</span></div><div class="category__intro"><p class="paragraph">' + formula[i]["intro"] +'</p><ul class="feature">';
+		var featureItem = '';
+		for (let k = 0; k < formula[i].feature.length; k++) {
+			featureItem += '<li>顏色: ' + formula[i].feature[k]["color"] +'</li>';
+			featureItem += '<li>Size: ' + formula[i].feature[k]["size"] +'</li>';
+			featureItem += '<li>Skill: ' + formula[i].feature[k]["skill"] +'</li>';
+		}	
+		formulaItem += featureItem;
+		formulaItem += '</ul></div></div><div class="category__body"><table class="category__table"><thead><tr><th>雙眼根數</th><th>定價</th><th>體驗價</th><th class="narrative">妝感說明</th></tr></thead><tbody>';
+		var priceGroup = '';
+		for (let l = 0; l < formula[i].price.length; l++) {
+			if(formula[i].price[l]["best"]== true) {
+				priceGroup += '<tr class="best"><td>' + formula[i].price[l]["amount"] +'</td><td><span class="cv didot">NT.</span><span class="value">' + formula[i].price[l]["fixed_price"] +'</span></td><td><span class="cv didot">NT.</span><span class="value">' + formula[i].price[l]["exp_price"] +'</span></td><td class="narrative">' + formula[i].price[l]["narrative"] +'</td></tr>';
+			} else {
+				priceGroup += '<tr><td>' + formula[i].price[l]["amount"] +'</td><td><span class="cv didot">NT.</span><span class="value">' + formula[i].price[l]["fixed_price"] +'</span></td><td><span class="cv didot">NT.</span><span class="value">' + formula[i].price[l]["exp_price"] +'</span></td><td class="narrative">' + formula[i].price[l]["narrative"] +'</td></tr>';
+			}
+		}
+		formulaItem += priceGroup;
+		formulaItem += '</tbody></table><div class="category__image"><img src="' + formula[i]["image"] + '" alt="' + formula[i]["alt"] + '"></div></div></div>';
+		formulaItem += '<div class="category__foot"><div class="enName didot">' + formula[i]["en"] + '</div><div class="chName"><div class="line"></div><div class="characteristic"><span>' + formula[i]["characteristic"] + '</span></div></div></div></div>';
+
+		formulaGroup += formulaItem;
+	}
+	$('#category').html(formulaGroup);
 });
---
+++ moji_lash/assets/js/price/formula.js
@@ -0,0 +1,97 @@
+var formula = [
+    {
+        "formula_en": "JP LASH",
+        "formula_ch": "日系無感嫁接",
+        "intro": "接完有根根分明的刷睫毛妝感<br>120-150根之間，擁有清新微淡妝-素顏的感覺。<br>200-250根之間，擁有假睫毛束束分明，閉眼有感。",
+        "feature": [{
+            "color": "亮面、100%黑色",
+            "size": "0.15",
+            "skill": "3D",
+        }],
+        "price": [
+            {
+                "amount": "100根",
+                "fixed_price": "1100",
+                "exp_price": "899",
+                "narrative": "微精神 真睫毛感",
+                "best": false
+            },
+            {
+                "amount": "120根",
+                "fixed_price": "1320",
+                "exp_price": "999",
+                "narrative": "淡淡睫毛膏",
+                "best": false
+            },
+            {
+                "amount": "150根",
+                "fixed_price": "1650",
+                "exp_price": "1299",
+                "narrative": "精神、長輩喜愛款",
+                "best": true
+            },
+            {
+                "amount": "180根",
+                "fixed_price": "1980",
+                "exp_price": "1499",
+                "narrative": "眼線+有厚度睫毛膏",
+                "best": false
+            },
+            {
+                "amount": "200根",
+                "fixed_price": "2200",
+                "exp_price": "1699",
+                "narrative": "自然款假睫毛",
+                "best": false
+            },
+            {
+                "amount": "250根",
+                "fixed_price": "2750",
+                "exp_price": "2199",
+                "narrative": "假睫毛款",
+                "best": false
+            },
+            {
+                "amount": "300根",
+                "fixed_price": "3300",
+                "exp_price": "2699",
+                "narrative": "濃密款假睫毛",
+                "best": false
+            }
+        ],
+        "image": "assets/images/img-category-01.jpg",
+        "alt": "JP LASH 日系無感嫁接",
+        "characteristic": "亮面 極黑",
+        "en": "<span>EYELASH</span>&nbsp;<span>EXTENSION</span>"
+    },
+    {
+        "formula_en": "USA REAL MINK",
+        "formula_ch": "美式好萊塢妝感",
+        "intro": "接完宛如天生的真睫毛，好氣色好精神的秘密武器。<br>120-150根之間，真假分不清、只知道更有神了。<br>200-250根之間，愛化妝的好幫手，再也不用裝自然款假睫毛。",
+        "feature": [{
+            "color": "霧面、80%黑色",
+            "size": "0.12",
+            "skill": "3D",
+        }],
+        "price": [
+            {
+                "amount": "120根",
+                "fixed_price": "1560",
+                "exp_price": "1299",
+                "narrative": "微精神 真睫毛感",
+                "best": true
+            },
+            {
+                "amount": "150根",
+                "fixed_price": "1950",
+                "exp_price": "1699",
+                "narrative": "淡淡睫毛膏",
+                "best": false
+            },
+        ],
+        "image": "assets/images/img-category-02.jpg",
+        "alt": "USA REAL MINK 美式好萊塢妝感",
+        "characteristic": "霧面 水貂",
+        "en": "<span>ROYAL REALMINK</span>&nbsp;<span>EXTENSION</span>"
+    }
+]
