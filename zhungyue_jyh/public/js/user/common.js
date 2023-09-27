8a00c68f2cdca9ba33bc1a986306fb05742baa3b
kohakubaibai <51185142+kohakubaibai@users.noreply.github.com>
2023-09-27 18:21:43 +0800

    create zhungyue project

---
+++ zhungyue_jyh/public/js/user/common.js
@@ -0,0 +1,23 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!********************************************!*\
+  !*** ./resources/assets/js/user/common.js ***!
+  \********************************************/
+$(function () {
+  console.log('jQuery');
+  headerHeight = $('header').outerHeight();
+  $('[data-js-item="anchor"]').on('click', function () {
+    if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate({
+          scrollTop: target.offset().top - headerHeight
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+/******/ })()
+;
