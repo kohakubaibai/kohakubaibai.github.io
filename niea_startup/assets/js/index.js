d539a668cc7a475bed933ef35def17da3ead445e
kohakubaibai <hakupai1990@gmail.com>
2024-05-16 01:37:33 +0800

    0516

--- niea_startup/assets/js/index.js
+++ niea_startup/assets/js/index.js
@@ -1,3 +1,28 @@
 $(function () {
-	
+	//輪播圖的設定
+    var mySwiper = new Swiper(".swiper-container", {
+        loop: true,
+        spaceBetween: 40,
+        slidesPerView: "auto",
+        centeredSlides: true,
+        autoResize: false,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        speed: 800,
+        autoplay: {
+            delay: 8000,
+        },
+        mousewheel: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    $('#myTab button').on('click', function (event) {
+        event.preventDefault()
+        $(this).tab('show')
+    });
 });
