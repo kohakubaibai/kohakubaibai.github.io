18ca865926d028ba46ffc85865a4ddb86fda08cd
kohakubaibai <hakupai1990@gmail.com>
2024-05-29 22:51:56 +0800

    add niea_startup_front

---
+++ niea_startup_front/assets/js/common.js
@@ -0,0 +1,126 @@
+$(function () {
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    //文字大小選單開關
+	$('.dropdown-toggle').dropdown();
+
+    //頁面滾動後header加入陰影
+    var header_h = $('.outsourcing_header').outerHeight(true);
+    $(window).on('scroll', function(){
+        var scrollTop = $(this).scrollTop();
+        if (scrollTop > header_h) {
+            $('.outsourcing_header').addClass('is-scroll');
+        } else {
+            $('.outsourcing_header').removeClass('is-scroll');
+        }
+    });
+
+
+    // 開關開啟主選單列表
+    $('#toggle-menu').on('click', function () {
+        $('body').addClass('openNav');
+        $('#header_nav').addClass('is-active');
+        $('.mmBtn').removeClass('is-active');
+        $(".mmContent").hide();
+    });
+    $('#close-menu').on('click', function () {
+        $('body').removeClass('openNav');
+        $('#header_nav').removeClass('is-active');
+        $('.mmBtn').removeClass('is-active');
+        $(".mmContent").hide();
+    });
+
+    $('.mmBtn').on('click mouseover', function (e) {
+        if (isMobile === true && e.type === 'mouseover') return;  
+        $(".mmContent").hide();
+        $(".mmBtn").removeClass("is-active");
+        $(this).toggleClass("is-active");
+        var id = $(this).attr("data-id");
+        $("#mmContent" + id).toggle();
+        $('[data-service-id="0"]').addClass('default');
+    })
+    $('.megamenu__item').on('mouseleave', function () {
+        if (isMobile === true) return;
+        $(".mmContent").hide();
+        $(".mmBtn").removeClass("is-active");
+        $('[data-service-id="0"]').addClass('default');
+    });
+
+    //主選單列表點空白處收起來
+    $('body').on('click', function (event) {
+        if (!$(event.target).is('.mmBtn')) {
+            $(".mmBtn").removeClass("is-active");
+            $(".mmContent").hide();
+        }
+    });
+
+    //分眾服務hover顯示&隱藏內容
+    var isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+    if (!isMobile){
+        $('[data-side-link="true"]').on('focus mouseover', function (e) {
+            e.preventDefault();
+            $(".sideMenu").hide();
+            $('[data-side-link="true"]').removeClass("is-active default");
+            $(this).toggleClass("is-active");
+            var id = $(this).attr("data-service-id");
+            $("#sideMenu" + id).show();
+        });
+        
+        $('.megamenu__item').mouseleave(function () {
+            $(".sideMenu").hide();
+            $('[data-side-link="true"]').removeClass("is-active");
+        });
+    }	
+
+    //搜尋框輸入後按鈕顯示
+    $('[data-form-element="input-search"]').on('keyup', function(){
+        var formItem = $(this).closest('[data-form-name="search"]');
+        var inputValue = $(this).val();
+        if (inputValue != '') {
+            formItem.find('[data-form-element="btn-clear"]').show();
+            formItem.find('[data-form-element="btn-submit"]').addClass('is-available');
+        } else {
+            formItem.find('[data-form-element="btn-clear"]').hide();
+            formItem.find('[data-form-element="btn-submit"]').removeClass('is-available');
+        }
+    });
+
+    //清除搜尋框
+    $('[data-form-element="btn-clear"]').on('click', function(e){
+        e.preventDefault();
+        var formItem = $(this).closest('[data-form-name="search"]');
+        formItem.find('[data-form-element="input-search"]').val('');
+        formItem.find('[data-form-element="btn-clear"]').hide();
+        formItem.find('[data-form-element="btn-submit"]').removeClass('is-available');
+    });
+
+    //開啟關閉popup
+    $('[data-js-id="openPopup"]').on('click', function(e){
+        e.preventDefault();
+        $('body').addClass('openPopup');
+        var target = $(this).data('popup-target');
+        $('[data-popup-name=' + target + ']').fadeIn();
+    });
+
+    $('[data-popup-close="true"]').on('click', function(){
+        $('body').removeClass('openPopup');
+        $(this).closest('[data-popup-id="group"]').fadeOut();
+        $(this).closest('[data-popup-id="group"]').find('input[type="text"],input[type="tel"],input[type="email"], textarea').val('');
+        $(this).closest('[data-popup-id="group"]').find('.formRow').removeClass('is-error');
+    });
+
+    //送出問題回饋表
+    $('[data-submit-id="report"]').on('click', function(e){
+        e.preventDefault();
+
+        //驗證表單必填欄位是否為空
+        var form = $(this).closest('form');
+
+        form.find('input[type="text"],input[type="tel"],input[type="email"], textarea').each(function(){
+            if($(this).val() == '') {
+                $(this).closest('.formRow').addClass('is-error');
+            } else {
+                //送出表單
+            }
+        });
+    });
+});
---
+++ niea_startup_front/assets/js/index.js
@@ -0,0 +1,122 @@
+$(function () {
+	//主圖輪播設定
+    var kvSwiper = new Swiper("#kvSwiper", {
+        loop: true,
+        spaceBetween: 40,
+        slidesPerView: "auto",
+        centeredSlides: true,
+        autoResize: false,
+        navigation: {
+            nextEl: "#kv-swiper-navigation_next",
+            prevEl: "#kv-swiper-navigation_prev",
+        },
+        speed: 800,
+        autoplay: {
+            delay: 8000,
+        },
+        mousewheel: true,
+        pagination: {
+            el: "#kv-swiper-pagination",
+            clickable: true,
+            dynamicBullets: false
+        },
+    });
+
+    //新創分眾服務指南tab設定
+    $('#serviceTabList button').on('click', function (event) {
+        event.preventDefault()
+        $(this).tab('show')
+    });
+
+    //最新動態tab設定
+    $('#newsTabList button').on('click', function (event) {
+        event.preventDefault()
+        $(this).tab('show')
+    });
+    //最新動態在手機模式下以下拉選單切換tab
+    $('[data-nav-select="true"]').on('change', function(){
+        var selectCate = $(this).find(':selected').val();
+        $('.outsourcingTab--news').find('.tab-pane').removeClass('active show');
+        $('#'+ selectCate).addClass('show active');
+    });
+
+    //主題策展輪播設定
+    var curationSwiper = new Swiper("#curationSwiper", {
+        autoResize: false,
+        mousewheel: true,
+        spaceBetween: 5,
+        speed: 800,
+        autoplay: {
+            delay: 8000,
+        },
+        navigation: {
+            nextEl: "#curation-swiper-navigation_next",
+            prevEl: "#curation-swiper-navigation_prev",
+        },
+        pagination: {
+            el: "#curation-swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            1199: {
+                loop: true,
+                slidesPerView: 1,
+                pagination: {
+                    dynamicBullets: false,
+                },
+            }
+            ,
+            1920: {
+                loop: false,
+                slidesPerView: 4,
+                watchOverflow: false,
+                pagination: {
+                    dynamicBullets: true,
+                    dynamicMainBullets: 1,
+                },
+            }
+        },
+    });
+
+    //推動成果數據數字動畫
+    var elementTop = $('.section--feature').offset().top;
+    var elementBottom = elementTop + $('.section--feature').height();
+    $(window).on('scroll', function(){
+        var pageTop = $(this).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+
+        if ((elementTop <= pageBottom) && (elementBottom >= pageTop)) {
+            $('[data-js-count="true"]').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).data('count')
+                }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+        }
+    });
+
+    //快速連結展開
+    $('[data-collapse-item="trigger"]').on('click', function(){
+        var target = $(this).data('collapse-target'),
+            isActive = $(this).hasClass('is-active'),
+            totalLength = $('[data-collapse-item="target"]').children('li').length;
+        if(isActive) {
+            $(this).removeClass('is-active').children('[data-collapse-item="title"]').text('展開');
+            $('[data-collapse-name="' + target + '"]').children('li').slice(8,totalLength).fadeOut(200);
+        } else {
+            $(this).addClass('is-active').children('[data-collapse-item="title"]').text('收合');
+            $('[data-collapse-name="' + target + '"]').children('li').slice(8,totalLength).fadeIn(200);
+        }
+    });
+
+    //浮動選單:捲動至最上方
+    $('[data-js-item="scrollTop"]').on('click', function(){
+        $('html, body').animate({scrollTop:0}, '300');
+        return false;
+    });
+});
