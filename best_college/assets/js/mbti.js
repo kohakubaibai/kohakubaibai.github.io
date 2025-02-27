708d5f67ebabcdbdd4767d76328c563f9f2411df
kohakubaibai <hakupai1990@gmail.com>
2025-02-27 14:29:27 +0800

    fix js

--- best_college/assets/js/mbti.js
+++ best_college/assets/js/mbti.js
@@ -148,40 +148,37 @@ const mbtiData = {
   ]
 };
 
-function generateContentItems(mbti) {
+function generateContentMbtis(mbtiCate) {
   const container = document.querySelector(`.mbtiSwiper .swiper-wrapper`);
-  mbtiData[mbti].forEach((item) => {
-    const newItem = document.createElement("div");
-    newItem.classList.add("swiper-slide");
-    newItem.innerHTML = `
+  mbtiData[mbtiCate].forEach((mbti) => {
+    const newMbti = document.createElement("div");
+    newMbti.classList.add("swiper-slide");
+    newMbti.innerHTML = `
       <div class="mbtiCard">
         <div class="mbtiCard__image">
           <div class="imgWrap">
             <div class="imgWrapper">
-              <img src="${item.imgSrc}" alt="${item.en}">
+              <img src="${mbti.imgSrc}" alt="${mbti.en}">
             </div>
           </div>
-          <div class="mbtiName ${item.class}">
-              <div class="chinese">${item.chinese}</div>
-              <div class="en">${item.en}</div>
+          <div class="mbtiName ${mbti.class}">
+              <div class="chinese">${mbti.chinese}</div>
+              <div class="en">${mbti.en}</div>
           </div>
         </div>
         <div class="mbtiCard__point">
           <div class="title">選志願時，你可以思考：</div>
           <ul class="list">
-            <li>${item.point1}</li>
-            <li>${item.point2}</li>
-            <li>${item.point3}</li>
+            <li>${mbti.point1}</li>
+            <li>${mbti.point2}</li>
+            <li>${mbti.point3}</li>
           </ul>
         </div>
       </div>
     `;
-    container.appendChild(newItem);
+    container.appendChild(newMbti);
   });
 }
 
 // 廣編學校_調用函數生成對應內容
-generateContentItems("mbti");
-generateContentItems("mbti2");
-generateContentItems("mbti3");
-generateContentItems("mbti4");
+generateContentMbtis("mbti");
