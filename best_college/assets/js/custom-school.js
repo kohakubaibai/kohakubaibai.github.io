645afacee3d8cb38d79e4c09acf64f9bda2c9ce6
kohakubaibai <hakupai1990@gmail.com>
2025-03-03 17:51:57 +0800

    0303 bak datas

--- best_college/assets/js/custom-school.js
+++ best_college/assets/js/custom-school.js
@@ -119,229 +119,188 @@ const contentData = {
   // 產學新典範
   cate1: [
     {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102774",
+      href: "https://www.cheers.com.tw/article/article.action?id=5104119",
       imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-659f4bd027166.jpg",
-      label: "航空",
-      title: "國立臺北科技大學",
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a5bcf85ef8b.jpg",
+      title: "徐有庠基金會",
     },
     {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102876",
+      href: "https://www.cheers.com.tw/article/article.action?id=5104113",
       imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202402/article-65c1d2af5065a.jpg",
-      label: "航空",
-      title: "致理科技大學",
-    },
-
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102865",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b2115982416.jpg",
-      label: "航空",
-      title: "景文科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102867",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b229fa93876.jpg",
-      label: "航空",
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a45756b265a.jpg",
       title: "元智大學",
     },
     {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102828",
+      href: "https://www.cheers.com.tw/article/article.action?id=5104183",
       imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65af2c9cc735e.jpg",
-      label: "航空",
-      title: "國立聯合大學",
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67bbf0e3af77a.jpg",
+      title: "國立宜蘭大學",
     },
     {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102822",
+      href: "https://www.cheers.com.tw/article/article.action?id=5104081",
       imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65af3ae22720e.jpg",
-      label: "航空",
-      title: "國立勤益科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102912",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202402/article-65d5871eef0e2.jpg",
-      label: "航空",
-      title: "大葉大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102858",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b718715faad.jpg",
-      label: "航空",
-      title: "崑山科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102857",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65ba244d90440.jpg",
-      label: "航空",
-      title: "遠東科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102863",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b8e0c37db46.jpg",
-      label: "航空",
-      title: "中信金融管理學院",
-    },
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202501/article-6790b3041ded6.jpg",
+      title: "亞東科技大學",
+    }
   ],
-  //   AI新思維
+  // AI新思維
   cate2: [
     {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102819",
+      href: "https://www.cheers.com.tw/talent/article.action?id=5104135",
       imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65afb1e19e11f.jpg",
-      label: "航空",
-      title: "世新大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102829",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b72b77277ee.jpg",
-      label: "航空",
-      title: "淡江大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102862",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b381a247fa9.jpg",
-      label: "航空",
-      title: "華梵大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102798",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65aa3a62116f5.jpg",
-      label: "航空",
-      title: "明新科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102866",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65af88ea64d60.jpg",
-      label: "航空",
-      title: "慈濟大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102847",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202402/article-65d07c0adfe09.jpg",
-      label: "航空",
-      title: "朝陽科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102827",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65afb1ba567c0.jpg",
-      label: "航空",
-      title: "東海大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102830",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b7149d9911d.jpg",
-      label: "航空",
-      title: "國立雲林科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102860",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b77a00bfcf8.jpg",
-      label: "航空",
-      title: "正修科技大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102890",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202402/article-65c1d67fb38a4.jpg",
-      label: "航空",
-      title: "義守大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102919",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202402/article-65d5870d69ba7.jpg",
-      label: "航空",
-      title: "亞洲大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102930",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202402/article-65dfd9fa33582.jpg",
-      label: "航空",
-      title: "南臺科技大學",
-    },
-  ],
-  //   推薦新亮點
-  cate3: [
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102824",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65afb1cd666d7.jpg",
-      label: "航空",
-      title: "銘傳大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102805",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65ae14af1f710.jpg",
-      label: "航空",
-      title: "長庚大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102868",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b12a134f3a4.jpg",
-      label: "航空",
-      title: "玄奘大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102895",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202402/article-65d016ed197a6.jpg",
-      label: "航空",
-      title: "靜宜大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102856",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65afb1f377ae4.jpg",
-      label: "航空",
-      title: "南華大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102861",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b12a2c9ea5a.jpg",
-      label: "航空",
-      title: "國立嘉義大學",
-    },
-    {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102823",
-      imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b21d5c79236.jpg",
-      label: "航空",
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67ab0c377d882.jpg",
       title: "長榮大學",
     },
     {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102859",
+      href: "https://www.cheers.com.tw/article/article.action?id=5104114",
       imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65b74597e42bf.jpg",
-      label: "航空",
-      title: "國立成功大學",
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a4595b24ae6.jpg",
+      title: "國立臺灣海洋大學",
     },
     {
-      href: "https://www.cheers.com.tw/article/article.action?id=5102864",
+      href: "https://www.cheers.com.tw/article/article.action?id=5104115",
       imgSrc:
-        "https://storage.googleapis.com/www-cheers-com-tw/article/202401/article-65ba1a3e08f36.jpg",
-      label: "航空",
-      title: "台南應用科技大學",
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a467fdaf86c.jpg",
+      title: "國立臺北大學",
     },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104134",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67ab06c6e6dba.jpg",
+      title: "輔仁大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104124",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a6d12ebb7bc.jpg",
+      title: "長庚科技大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104149",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67aea529c3ece.jpg",
+      title: "東海大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104142",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67ad5b4b6b2ea.jpg",
+      title: "國立雲林科技大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104116",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a4852ff251a.jpg",
+      title: "國立臺南大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104060",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202501/article-678e108043e1d.jpg",
+      title: "明新科技大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104184",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67bc1b076d06c.jpg",
+      title: "亞洲大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104042",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202501/article-67870c1d2fd09.jpg",
+      title: "正修科技大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104159",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67b45520cb8d3.jpg",
+      title: "東吳大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104160",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67b444f0c6299.jpg",
+      title: "淡江大學",
+    }
+  ],
+  // 亮點新趨勢
+  cate3: [
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104123",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a6c1cfb29e1.jpg",
+      title: "朝陽科技大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104047",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202501/article-6788854b0ef21.jpg",
+      title: "玄奘大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104186",
+      imgSrc:
+        "https://storage.googleapis.com/dev-www-cheers-com-tw/article/202502/article-67aaee8b85304.jpg",
+      title: "致理大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104164",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67b56d59b9700.jpg",
+      title: "高雄醫學大學口腔系",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104158",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67b408bb3b82f.jpg",
+      title: "高雄醫學大學運動醫學系",
+    },
+    {
+      href: "https://www.cw.com.tw/article/5133877",
+      imgSrc:
+        "https://cdn-www.cw.com.tw/article/202501/article-6791ed9fbec969.21701853.jpg",
+      title: "日本台灣交流協會",
+    }
+  ],
+  // 培育新視角
+  cate4: [
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104128",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a99ef455a0d.jpg",
+      title: "南華大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104131",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67aaa82b023db.jpg",
+      title: "明志大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104122",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67a6b6fd4b31d.jpg",
+      title: "長庚大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104053",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202501/article-678a110b53a0e.jpg",
+      title: "臺北醫學大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104052",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202501/article-67909d1ee055f.jpg",
+      title: "銘傳大學",
+    },
+    {
+      href: "https://www.cheers.com.tw/article/article.action?id=5104161",
+      imgSrc:
+        "https://storage.googleapis.com/www-cheers-com-tw/article/202502/article-67b45491bc72f.jpg",
+      title: "慈濟大學",
+    }
   ],
 };
 
@@ -357,7 +316,6 @@ function generateContentItems(category) {
               <img src="${item.imgSrc}" alt="${item.title}">
           </div>
           <div class="txtWrap">
-            <h4 class="p-adlist__label">${item.label}</h4>
             <h3 class="p-adlist__title">${item.title}</h3>
           </div>
       </a>
@@ -367,6 +325,7 @@ function generateContentItems(category) {
 }
 
 // 廣編學校_調用函數生成對應內容
-// generateContentItems("cate1");
-// generateContentItems("cate2");
-// generateContentItems("cate3");
+generateContentItems("cate1");
+generateContentItems("cate2");
+generateContentItems("cate3");
+generateContentItems("cate4");
