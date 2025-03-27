21774e6e2f9e6058f041c9ef0f600f8d73a5c212
kohakubaibai <hakupai1990@gmail.com>
2025-03-27 17:51:01 +0800

    0327 dropdown fix

--- cheers-DEI/2025_dei/assets/js/custom.js
+++ cheers-DEI/2025_dei/assets/js/custom.js
@@ -1,32 +1,38 @@
 //navbar scroll
 $(function () {
-  $(document).scroll(function () {
-    var $nav = $(".l-navbar");
-    $nav.toggleClass("l-navbar--scrolled", $(this).scrollTop() > $nav.height());
-  });
+	$(document).scroll(function () {
+		var $nav = $(".l-navbar");
+		$nav.toggleClass("l-navbar--scrolled", $(this).scrollTop() > $nav.height());
+	});
 });
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function () {
-  $(".js-anchor a").on("click", function (event) {
-    var $anchor = $(this);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $($anchor.attr("href")).offset().top,
-        },
-        1500,
-        "easeInOutExpo"
-      );
-    event.preventDefault();
+	$(".js-anchor a").on("click", function (event) {
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
 
-    $(".l-navbar__menu ").removeClass("open");
-    $(".l-navbar__toggler--activate").attr("class", "l-navbar__toggler");
-  });
+			// Store hash
+			var hash = this.hash;
 
-  // Toggle dropdown
-	$('.js-selectToggler').on('click', function(e) {
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function () {
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+
+			$(".l-navbar__menu ").removeClass("open");
+			$(".l-navbar__toggler--activate").attr("class", "l-navbar__toggler");
+		} // End if
+
+	});
+
+	// Toggle dropdown
+	$('.js-selectToggler').on('click', function (e) {
 		e.preventDefault();
 		$(this).toggleClass('active');
 		$('.js-selectOption').slideToggle(200);
@@ -39,10 +45,10 @@ new WOW().init();
 
 //overlay navbar
 $(document).ready(function () {
-  $(".l-navbar__toggler").click(function () {
-    $(".l-navbar__menu").toggleClass("open");
-    $(this)
-      .toggleClass("l-navbar__toggler")
-      .toggleClass("l-navbar__toggler--activate");
-  });
+	$(".l-navbar__toggler").click(function () {
+		$(".l-navbar__menu").toggleClass("open");
+		$(this)
+			.toggleClass("l-navbar__toggler")
+			.toggleClass("l-navbar__toggler--activate");
+	});
 });
