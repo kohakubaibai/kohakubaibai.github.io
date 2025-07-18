e0fc72f12a69a249f2614ca8f845a65c05952a1a
kohakubaibai <hakupai1990@gmail.com>
2025-07-18 18:32:05 +0800

    0718 sleep

--- cw_pressure-sleep/assets/js/common.js
+++ cw_pressure-sleep/assets/js/common.js
@@ -17,30 +17,32 @@ function calculateScore() {
     }
 
     // 顯示結果
-    const resultDiv = document.getElementById('result');
-    const totalScoreSpan = document.getElementById('totalScore');
+    const resultDiv = document.getElementById('resultBox');
     const resultTitle = document.getElementById('resultTitle');
+    const scoreRange = document.getElementById('scoreRange');
     const resultDescription = document.getElementById('resultDescription');
-
-    totalScoreSpan.textContent = totalScore;
 
     // 根據分數給出評估結果
     resultDiv.className = 'result';
     if (totalScore <= 13) {
         resultDiv.classList.add('low');
         resultTitle.textContent = '輕微壓力型';
+        scoreRange.textContent = '0~13分'
         resultDescription.innerHTML = '<p>你的壓力還在可以接受的範圍，善於調適壓力的你，對高壓環境適應度很高！</p><p>建議可以先嘗試看看每週2次以上的運動緩解壓力，多曬曬太陽也能幫助睡眠。</p>';
     } else if (totalScore >= 14 && totalScore <= 26 ) {
         resultDiv.classList.add('medium');
         resultTitle.textContent = '中度壓力型';
+        scoreRange.textContent = '14~26分'
         resultDescription.innerHTML = '<p>你成功將生活壓力調適在可控範圍內，是個勤奮的生活家！</p><p>但別忘了與家人、朋友聊聊，或多關注壓力管理、學習紓壓來調適壓力，不然長久累積下來，可能會常睡不好，導致睡眠債會越積越多。</p><p>想拒絕壓力型失眠，體驗深度熟睡，繼續往下看就對了！</p>';
     } else if (totalScore >= 27 && totalScore <= 37) {
         resultDiv.classList.add('high');
         resultTitle.textContent = '高度壓力型';
+        scoreRange.textContent = '27~37分'
         resultDescription.innerHTML = '<p>生活的高壓讓你有點喘不過氣。你已經很努力了，卻還是感到很辛苦嗎？</p><p>其實你不孤單，現代快節奏的社會，許多人會藉由找尋合格的心理衛生專業人員來紓解內心壓力，或是補充好眠保健食品來對抗壓力型失眠。</p><p>畢竟睡得好才能補充明天所需的精神，勇敢為自己的健康做出嘗試吧！</p>';
     } else {
         resultDiv.classList.add('over');
         resultTitle.textContent = '過度壓力型';
+        scoreRange.textContent = '38分以上'
         resultDescription.innerHTML = '<p>你一定很辛苦吧？</p><p>嚴重的壓力已經讓你提不起力氣面對未來，你可以尋求專業心理衛生人員、精神專科醫師的幫助。</p><p>除了透過醫師專用處方用藥、心理治療，也可以試試看添加天然草本類型的輔眠保健品，睡飽、吃好，趕快讓生活回歸健康軌道！</p>';
     }
 
--- cw_pressure-sleep/assets/js/index.js
+++ cw_pressure-sleep/assets/js/index.js
@@ -1,162 +1,218 @@
-$(function() {
+$(function () {
+	/************************** ScrollMagic start ******************************/
+	var hash = location.hash;
+	var navLink = $(".navbar .nav-link");
+	var controller = new ScrollMagic.Controller();
+	var scrollArea = $(".scrollArea");
+	var scrollAreaIdArr = [];
+	var scrollAreaActive;
+	function updateNav(href, andPush) {
+		if (andPush) {
+			history.pushState({ href: href }, "", href);
+		} else {
+			history.replaceState({ href: href }, "", href);
+		}
+		scrollAreaActive = href.slice(1);
+	}
+	scrollArea.each(function (i) {
+		var area = $(this);
+		var id = area.attr("id");
+		scrollAreaIdArr.push(id);
+		var scene = new ScrollMagic.Scene({
+			triggerElement: area.get(0),
+		})
+			.on("enter", function (e) {
+				updateNav("#" + id, false);
+			})
+			.on("leave", function (e) {
+				if (i > 0 && e.scrollDirection == "REVERSE") {
+					updateNav(
+						"#" +
+						scrollAreaIdArr[
+						scrollAreaIdArr.findIndex((el) => el == scrollAreaActive) - 1
+						],
+						false
+					);
+				}
+			})
+			.addTo(controller);
+	});
 
-	var slickBasicSetting={
-		dots: true,
+	setTimeout(function () {
+		hash && navLink.filter("[href$='" + hash + "']").trigger("click");
+	}, 100);
+	/************************** ScrollMagic end ******************************/
+
+	/************************** game start ******************************/
+	let currentQuestion = 1;
+	const totalQuestions = 10;
+
+	function initializeTest() {
+		$(".quizBox").each(function (index) {
+			const $this = $(this);
+			const questionNum = index + 1;
+
+			if (questionNum === 1) {
+				$this.show().addClass("active");
+			} else {
+				$this.hide().removeClass("active");
+			}
+		});
+
+		$(".stressForm__result").hide();
+
+		bindEvents();
+	}
+
+	function bindEvents() {
+		$(".stressForm__questions").on(
+			"change",
+			'input[type="radio"]',
+			function () {
+				const $this = $(this);
+				const questionName = $this.attr("name");
+				const questionNum = parseInt(questionName.replace("q", ""));
+
+				handleAnswer(questionNum);
+			}
+		);
+	}
+
+	function handleAnswer(questionNumber) {
+		setTimeout(() => {
+			if (questionNumber < totalQuestions) {
+				showNextQuestion(questionNumber);
+			} else {
+				calculateAndShowResult();
+			}
+		}, 300);
+	}
+
+	function showNextQuestion(currentQ) {
+		const $currentQuestionBox = $(`[data-question="${currentQ}"]`);
+		const $nextQuestionBox = $(`[data-question="${currentQ + 1}"]`);
+
+		if ($currentQuestionBox.length && $nextQuestionBox.length) {
+			$currentQuestionBox.fadeOut(300, function () {
+				$(this).removeClass("active");
+
+				$nextQuestionBox.addClass("active").fadeIn(300);
+			});
+
+			currentQuestion = currentQ + 1;
+		}
+	}
+
+	function calculateAndShowResult() {
+		let totalScore = 0;
+		let answeredQuestions = 0;
+
+		for (let i = 1; i <= totalQuestions; i++) {
+			const $selectedRadio = $(`input[name="q${i}"]:checked`);
+			if ($selectedRadio.length) {
+				totalScore += parseInt($selectedRadio.data("score"));
+				answeredQuestions++;
+			}
+		}
+
+		if (answeredQuestions < totalQuestions) {
+			console.log("測試出現錯誤，請重新開始！");
+			return;
+		}
+
+		const $lastQuestionBox = $(`[data-question="${totalQuestions}"]`);
+		$lastQuestionBox.fadeOut(300, function () {
+			$(this).removeClass("active");
+			showResult(totalScore);
+		});
+	}
+
+	function showResult(totalScore) {
+		const $resultSection = $(".stressForm__result");
+		const $resultDiv = $("#resultBox");
+		const $resultTitle = $("#resultTitle");
+		const $scoreRange = $("#scoreRange");
+		const $resultDescription = $("#resultDescription");
+
+		$resultDiv.removeClass().addClass("resultBox");
+
+		const resultData = getResultData(totalScore);
+
+		$resultDiv.addClass(resultData.class);
+		$resultTitle.text(resultData.title);
+		$scoreRange.text(resultData.range);
+		$resultDescription.html(resultData.description);
+
+		$resultSection.fadeIn(500);
+	}
+
+	function getResultData(totalScore) {
+		if (totalScore <= 13) {
+			return {
+				class: "low",
+				title: "輕微壓力型",
+				range: "0~13分",
+				description:
+					"<p>你的壓力還在可以接受的範圍，善於調適壓力的你，對高壓環境適應度很高！</p><p>建議可以先嘗試看看每週2次以上的運動緩解壓力，多曬曬太陽也能幫助睡眠。</p>",
+			};
+		} else if (totalScore >= 14 && totalScore <= 26) {
+			return {
+				class: "medium",
+				title: "中度壓力型",
+				range: "14~26分",
+				description:
+					"<p>你成功將生活壓力調適在可控範圍內，是個勤奮的生活家！</p><p>但別忘了與家人、朋友聊聊，或多關注壓力管理、學習紓壓來調適壓力，不然長久累積下來，可能會常睡不好，導致睡眠債會越積越多。</p><p>想拒絕壓力型失眠，體驗深度熟睡，繼續往下看就對了！</p>",
+			};
+		} else if (totalScore >= 27 && totalScore <= 37) {
+			return {
+				class: "high",
+				title: "高度壓力型",
+				range: "27~37分",
+				description:
+					"<p>生活的高壓讓你有點喘不過氣。你已經很努力了，卻還是感到很辛苦嗎？</p><p>其實你不孤單，現代快節奏的社會，許多人會藉由找尋合格的心理衛生專業人員來紓解內心壓力，或是補充好眠保健食品來對抗壓力型失眠。</p><p>畢竟睡得好才能補充明天所需的精神，勇敢為自己的健康做出嘗試吧！</p>",
+			};
+		} else {
+			return {
+				class: "over",
+				title: "過度壓力型",
+				range: "38分以上",
+				description:
+					"<p>你一定很辛苦吧？</p><p>嚴重的壓力已經讓你提不起力氣面對未來，你可以尋求專業心理衛生人員、精神專科醫師的幫助。</p><p>除了透過醫師專用處方用藥、心理治療，也可以試試看添加天然草本類型的輔眠保健品，睡飽、吃好，趕快讓生活回歸健康軌道！</p>",
+			};
+		}
+	}
+
+	initializeTest();
+	/************************** game end ******************************/
+
+	/************************** slick start ******************************/
+	var slickBasicSetting = {
+		dots: false,
 		//autoplay: true,
 		autoplaySpeed: 5000,
 		infinite: true,
-		prevArrow:'<div class="btn-arrowL effect-moveL"><i class="fas fa-caret-left"></i></div>',
-		nextArrow:'<div class="btn-arrowR effect-moveR"><i class="fas fa-caret-right"></i></div>'
+		prevArrow:
+			'<div class="btn-arrowL effect-moveL"></div>',
+		nextArrow:
+			'<div class="btn-arrowR effect-moveR"></div>',
 	};
-	var slickArticleSetting={
+
+	var slickArticleSetting = {
 		slidesToShow: 3,
-		slidesToScroll: 3,
+		slidesToScroll: 1,
 		responsive: [
 			{
-
-				breakpoint: 767, 
+				breakpoint: 767,
 				settings: {
 					slidesToShow: 1,
-					slidesToScroll: 1
-				}
-			}
-		]
+					slidesToScroll: 1,
+				},
+			},
+		],
 	};
-	var slick=$(".slick");
-	var slickArticle=$(".slickArticle");
-	slick.slick(slickBasicSetting);
+
+	var slickArticle = $(".js-articleSlick");
 	slickArticle.slick($.extend({}, slickBasicSetting, slickArticleSetting));
-	
-	/************************** ScrollMagic start ******************************/
-	var hash=location.hash;
-	var navLink=$(".navbar .nav-link");
-	var controller=new ScrollMagic.Controller();
-	var scrollArea=$(".scrollArea");
-	var scrollAreaIdArr=[];
-	var scrollAreaActive;
-	function updateNav(href, andPush){
-		if(andPush){
-			history.pushState({href:href}, "", href);
-		}else{
-			history.replaceState({href:href}, "", href);
-		}
-		scrollAreaActive=href.slice(1);
-	}
-	scrollArea.each(function(i){
-		var area=$(this);
-		var id=area.attr("id");
-		scrollAreaIdArr.push(id);
-		var scene=new ScrollMagic.Scene({
-			triggerElement: area.get(0)
-		}).on("enter", function(e) {
-			updateNav("#"+id, false);
-		}).on("leave", function(e) {
-			if(i>0 && e.scrollDirection=="REVERSE"){
-				updateNav("#"+scrollAreaIdArr[scrollAreaIdArr.findIndex(el => el==scrollAreaActive)-1], false);
-			}
-		}).addTo(controller);
-	});
-	/************************** ScrollMagic end ******************************/
-	
-	/************************** game start ******************************/
-	function initGame(){
-		var score;
-		var nowQ;
-		var gameBox=$(".gameBOX").data("correctAudio", new Audio("imgs/gameO.mp3")).data("wrongAudio", new Audio("imgs/gameX.mp3"));
-		var QArr=["Y","N","Y","Y","N"];
-		var Q=gameBox.find(".Q");
-		var QAmount=Q.length;
-		var YN=Q.find("button");
-		var ABlock=gameBox.find(".ABlock");
-		var RBlock=gameBox.find(".RBlock");
-		var R=RBlock.find(".R");
-		var resetBtn=R.find("button");
-		
-		function showNext(){
-			var tmpNext=nowQ+1;
-			nowQ=(tmpNext>QAmount? 0 : tmpNext);
-			ABlock.removeClass("show correct wrong");
-			if(nowQ){
-				Q.eq(nowQ-1).addClass("active").siblings().removeClass("active");
-			}else{
-				let r=(function(){
-					if(score<=2){
-						return 1;
-					}else if(score>=3 && score<=4){
-						return 2;
-					}else if(score>=5){
-						return 3;
-					}
-				})();
-				R.eq(r-1).addClass("active");
-				RBlock.addClass("show");
-			}
-		}
-		
-		function reset(){
-			score=0;
-			nowQ=1;
-			Q.eq(nowQ-1).addClass("active").siblings().removeClass("active");
-			YN.removeClass("out");
-			ABlock.removeClass("show correct wrong");
-			RBlock.removeClass("show");
-			R.removeClass("active");
-		}
-		reset();
 
-		Q.each(function(i){
-			var _Q=$(this);
-			var answer=QArr[i];
-			var btn=_Q.find("button");
-			var correctBtn=btn.filter("."+answer);
-			var wrongBtn=correctBtn.siblings("button");
-			var cw;
-			var audio;
-			btn.each(function(){
-				var _btn=$(this);
-				var _answer=_btn.attr("class").split(" ")[0];
-				_btn.click(function(){
-					cw=(_answer==answer)? "correct" : "wrong";
-					ABlock.addClass("show "+cw);
-					wrongBtn.addClass("out");
-					if(cw=="correct"){
-						score++;
-					}
-					audio=gameBox.data(cw+"Audio");
-					audio.currentTime=0;
-					audio.play();
-					setTimeout(function(){
-						showNext();
-					}, 1200); //對錯公布的停留秒數
-				});
-			});
-		});
-
-		resetBtn.click(function(){
-			reset();
-		});
-	}
-	initGame();
-	/************************** game end ******************************/
-	
-	/************************** 院所內容 start ******************************/
-	var navbarX=$(".navbarX");
-	var hospitalCT=$(".hospitalCT");
-	var liAmountMaxDefault=navbarX.is(":visible")? 6 : 9;
-	hospitalCT.each(function(){
-		var ct=$(this);
-		var li=ct.find("li");
-		var liAmount=li.length;
-		var more=ct.find(".more");
-		more.click(function(){
-			ct.addClass("showAll");
-		});
-		liAmount<=liAmountMaxDefault && more.trigger("click");
-	});
-	/************************** 院所內容 end ******************************/
-	
-	setTimeout(function(){
-		hash && navLink.filter("[href$='"+hash+"']").trigger("click");
-	}, 100);
-	
+	/************************** slick end ******************************/
 });
